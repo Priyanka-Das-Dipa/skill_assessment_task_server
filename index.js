81c0036309fa8e7b6f29939b1ba6b58948c04f93
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-02-27 03:27:22 +0600

    working

--- index.js
+++ index.js
@@ -9,10 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_USERPASSWORD}@cluster0.mfd0sli.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +25,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const paymentCollection = client.db("paymentDB").collection("payment");
+    const userCollection = client.db("paymentDB").collection("users");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+    //   console.log(user);
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/getUser", async(req, res) => {
+        const email = req.body;
+        const query = {email:email};
+        console.log(email);
+        const result = await userCollection.findOne(query);
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,7 +57,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
